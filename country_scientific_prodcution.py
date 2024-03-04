d33ca833db14c3f93891c309d408a69b65dfc2fb
Juan Jose Marin Alvarez <102542272+Thepuffyn@users.noreply.github.com>
2024-03-04 09:04:04 -0500

    Update country_scientific_prodcution.py

--- country_scientific_prodcution.py
+++ country_scientific_prodcution.py
@@ -10,3 +10,37 @@ def load_affiliations():
         index_col=None,
     )[["Affiliations"]]
     return dataframe
+  
+def remove_na_rows(affiliations):
+    """Elimina las filas con valores nulos en la columna 'Affiliations'"""
+
+    affiliations = affiliations.copy()
+    affiliations = affiliations.dropna(subset=["Affiliations"])
+
+    return affiliations
+  
+
+df = load_affiliations()
+df = remove_na_rows(df)
+for i in range(5):
+    print(df.Affiliations.values[i])
+  
+  
+def add_countries_column(affiliations):
+    """Transforma la columna 'Affiliations' a una lista de paises."""
+
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["Affiliations"].copy()
+    affiliations["countries"] = affiliations["countries"].str.split(";")
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y.split(",") for y in x]
+    )
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y[-1].strip() for y in x]
+    )
+    affiliations["countries"] = affiliations["countries"].map(set)
+    affiliations["countries"] = affiliations["countries"].str.join(", ")
+
+    return affiliations
+  
+  
