276eb5157b5705e8674644f97ff46a5c3f580c23
Juan Jose Marin Alvarez <102542272+Thepuffyn@users.noreply.github.com>
2024-03-04 08:46:09 -0500

    Update country_scientific_prodcution.py

--- country_scientific_prodcution.py
+++ country_scientific_prodcution.py
@@ -1,1 +1,12 @@
 """Taller Presencial Evaluable"""
+
+import pandas as pd
+
+def load_affiliations():
+    """Carga el archivo scopus-papers.csv y retorna un dataframe con la columna 'Affiliations'"""
+    dataframe = pd.read_csv(
+        "https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/scopus-papers.csv",
+        sep=",",
+        index_col=None,
+    )[["Affiliations"]]
+    return dataframe
